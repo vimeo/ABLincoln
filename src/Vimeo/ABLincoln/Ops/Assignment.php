2fc107dde61c0e004d3ae24f45fc4d65c99ee6e3
Alex Kalicki <alexander.kalicki@gmail.com>
2014-06-05 18:07:21 -0400

    added implementation of assignment class

--- src/Vimeo/ABLincoln/Ops/Assignment.php
+++ src/Vimeo/ABLincoln/Ops/Assignment.php
@@ -13,23 +13,36 @@ class Assignment implements ArrayAccess
         $this->data = array();
     }
 
-    public function offsetExists()
+    public function evaluate($value)
     {
-
+        return $value;
     }
 
-    public function offsetGet()
+    public function offsetExists($offset)
     {
-
+        return isset($this->data[$offset]);
     }
 
-    public function offsetSet()
+    public function offsetGet($offset)
     {
-
+        return isset($this->data[$offset]) ? $this->data[$offset] : null;
     }
 
-    public function offsetUnset()
+    public function offsetSet($offset, $value)
     {
-        
+        if ($value instanceof AbOpRandom) {
+            if (!array_key_exists('salt', $value->args)) {
+                $value->args['salt'] = $offset;
+            }
+            $this->data[$offset] = $value->execute($this);
+        }
+        else {
+            $this->data[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
     }
 }
