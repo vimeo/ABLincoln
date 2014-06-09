a1f072d957272658032c7b5c6ef812cbf1aa86d5
Alex Kalicki <alexander.kalicki@gmail.com>
2014-06-09 11:51:53 -0400

    initial version of assignment test using phpunit

---
+++ tests/AssignmentTest.php
@@ -0,0 +1,38 @@
+<?php
+
+class AssignmentTest extends PHPUnit_Framework_TestCase
+{
+    private $tester_salt = 'test_salt';
+
+    public function testOffsetSetUnset()
+    {
+        $a = new Assignment($this->tester_salt);
+        $this->assert(!isset($a[0]));
+        $a[0] = 5;
+        $this->assert(isset($a[0]));
+        unset($a[0]);
+        $this->assert(!isset($a[0]);
+    }
+
+    public function testSetGetConstant()
+    {
+        $a = new Assignment($this->tester_salt);
+        $a[0] = 5;
+        $this->assertEquals($a[0], 5);
+    }
+
+    public function testSetGetString()
+    {
+        $a = new Assignment($this->tester_salt);
+        $a['test'] = 'a';
+        $this->assertEquals($a['test'], 'a');
+    }
+
+    public function testSetGetNull()
+    {
+        $a = new Assignment($this->tester_salt);
+        $a[12] = 5;
+        $a[] = 'a';
+        $this->assertEquals($a[13], 'a');
+    }
+}
