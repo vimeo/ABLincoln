d7ece133020394459aad34a66da491d5567bc6aa
Alex Kalicki <alexander.kalicki@gmail.com>
2014-06-10 17:32:26 -0400

    added Simple experiment documentation

--- src/Vimeo/ABLincoln/Experiments/Simple.php
+++ src/Vimeo/ABLincoln/Experiments/Simple.php
@@ -4,25 +4,49 @@ namespace Vimeo\ABLincoln\Experiments;
 
 use \Psr\Log\LoggerInterface;
 
+/**
+ * Simple experiment base class which exposure logs according to PSR logging
+ * specifications. User experiments extending this class should pass in their
+ * own compatible logger instance.
+ */
 class Simple extends AbstractExperiment
 {
     protected $logger;
     const LOG_FORMAT = '%s with event type: %s';å
 
+    /**
+     * Construct a new Simple experiment, passing in hashing inputs and logger
+     *
+     * @param array $inputs array of inputs to use for experiment hashing
+     * @param LoggerInterface $logger optional PSR logging instance to use
+     */
     public function __construct($inputs, LoggerInterface $logger = null)
     {
         parent::__construct($inputs);
         $this->logger = $logger;
     }
 
+    /**
+     * Set a new PSR logging instance to use for output
+     *
+     * @param LoggerInterface $logger PSR-compliant logger to use for output
+     */
     public function setLogger(LoggerInterface $logger)
     {
         $this->logger = $logger;
     }
 
+    /**
+     * All logger configuring will be done outside of this class
+     */
     protected function configureLogger() {}
 
-    public function log($data)
+    /**
+     * Use the logging instance to log experiment name, event type, and data
+     *
+     * @param array $data exposure log data to record
+     */
+    protected function log($data)
     {
         if (isset($logger)) {
             $this->logger->info(sprintf(self::LOG_FORMAT, $this->name,
@@ -30,7 +54,10 @@ class Simple extends AbstractExperiment
         }
     }
 
-    public function previouslyLogged()
+    /**
+     * Assume data has never been logged before for a Simple experiment
+     */
+    protected function previouslyLogged()
     {
         return false;
     }
