3ed8b30edb67ad624c638f3db896719b78c55794
Alex Kalicki <alexander.kalicki@gmail.com>
2014-06-11 10:57:55 -0400

    added abstract namespace skeleton

---
+++ src/Vimeo/ABLincoln/Namespaces/AbstractNamespace.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Vimeo\ABLincoln\Namespaces;
+
+abstract class AbstractNamespace
+{
+    public function __contruct() {}
+
+    abstract public function inExperiment();
+
+    abstract public function addExperiment($name, $expObject, $numSegments, $inputs);
+
+    abstract public function removeExperiment($name);
+
+    abstract public function setAutoExposureLogging($value);
+
+    abstract public function get($name, $default);
+
+    abstract public function logExposure($extras = null);
+
+    abstract public function logEvent($eventType, $extras = null);
+}
