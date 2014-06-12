f83a558a7082e630d9ac67b72e63e1d1dadd63d0
Alex Kalicki <alexander.kalicki@gmail.com>
2014-06-12 16:38:22 -0400

    added unit tests for basic experiment objects

---
+++ tests/ExperimentTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use \Vimeo\ABLincoln\Experiments\AbstractExperiment;
+use \Vimeo\ABLincoln\Operators\Random as Random;
+
+$global_log = array();
+
+/**
+ * PHPUnit Experiment test class
+ */
+class ExperimentTest extends \PHPUnit_Framework_TestCase
+{
+    public function testVanillaExperiment()
+    {
+        global $global_log;
+        $userid = 42; $username = 'a_name';
+
+        $e = new TestVanillaExperiment($userid);
+        $params = $e->getParams();
+        $this->assertTrue(array_key_exists('foo', $params));
+        $this->assertEquals($params['foo'], 'b');
+        $this->assertEquals(count($global_log), 1);
+
+        $e = new TestVanillaExperiment(array($userid, $username));
+        $params = $e->getParams();
+        $this->assertTrue(array_key_exists('foo', $params));
+        $this->assertEquals($params['foo'], 'a');
+        $this->assertEquals(count($global_log), 2);
+    }
+}
+
+class TestVanillaExperiment extends AbstractExperiment
+{
+    protected function setup()
+    {
+        $this->name = 'test_name';
+    }
+
+    protected function assign($params, $inputs)
+    {
+        $params['foo'] = new Random\UniformChoice(array(
+            'choices' => array('a', 'b'),
+        ), $inputs);
+    }
+
+    protected function previouslyLogged()
+    {
+        return false;
+    }
+
+    protected function configureLogger() {}
+
+    protected function log($data)
+    {
+        global $global_log;
+        $global_log[] = $data;
+    }
+}
