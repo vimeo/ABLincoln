16a41a1e1a97f2326aeae65febe798fe1106fe6e
Alex Kalicki <alexander.kalicki@gmail.com>
2015-01-06 03:51:40 -0500

    add documentation for new simpleExperiment class

--- src/Vimeo/ABLincoln/Experiments/SimpleExperiment.php
+++ src/Vimeo/ABLincoln/Experiments/SimpleExperiment.php
@@ -2,6 +2,9 @@
 
 namespace Vimeo\ABLincoln\Experiments;
 
+/*
+ * Simple experiment base class which exposure logs to a file
+ */
 abstract class SimpleExperiment extends AbstractExperiment
 {
     use Logging\FileLoggerTrait;
